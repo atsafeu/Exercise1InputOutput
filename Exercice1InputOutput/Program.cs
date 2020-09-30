8db112cc9264c59de276e5e95ad28cc3546eee31
Armand Tsafack <armandtsafack@armandtsafack.home>
2020-09-30 17:23:06 -0500

    Exercise1 InputOuputSolution created

---
+++ Exercice1InputOutput/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercice1InputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
